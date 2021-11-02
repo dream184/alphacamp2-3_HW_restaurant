f4fe6f2c0e3b6a98ba0be6459a0488834a5902c5
shibao <dream0184@yahoo.com.tw>
2021-11-02 19:06:09 +0800

    feat: install express, express-handlebars

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.listen(port, (req, res) => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
