2de1b9e51a66970ac0e6fc01b17903cf119b6f7c
shibao <dream0184@yahoo.com.tw>
2021-11-05 22:55:18 +0800

    feat: add new restaurant function and new's view

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 const app = express()
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list')
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-const restaurantList = require('./restaurant.json')
   
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant =>
-    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) 
-  )
-  res.render('index', { restaurantList: restaurants, keyword })
+const Restaurant = require('./models/restaurant')
+
+
+// app.get('/search', (req, res) => {
+//   const keyword = req.query.keyword
+//   const restaurants = restaurantList.results.filter(restaurant =>
+//     restaurant.name.toLowerCase().includes(keyword.toLowerCase()) 
+//   )
+//   res.render('index', { restaurantList: restaurants, keyword })
+// })
+
+// app.get('/restaurants/:id', (req, res) => {
+//   const restaurantId = req.params.id
+//   const restaurant = []
+//   const foundedRestaurant = restaurantList.results.find(restaurant => restaurant.id === Number(restaurantId))
+//   restaurant.push(foundedRestaurant)
+//   res.render('show', { restaurant })
+// })
+
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
 })
 
-app.get('/restaurants/:id', (req, res) => {
-  const restaurantId = req.params.id
-  const restaurant = []
-  const foundedRestaurant = restaurantList.results.find(restaurant => restaurant.id === Number(restaurantId))
-  restaurant.push(foundedRestaurant)
-  res.render('show', { restaurant })
+app.post('/restaurants/', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, description }  = req.body
+  Restaurant.create({ name, name_en, category, image, location, phone, google_map, description })
+    .then(() => res.redirect('/'))
+    .catch(error =>
+      console.log(error)
+    )
 })
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurantList: restaurants }))
+    .catch(error => console.error('error'))
 })
 
 app.listen(port, (req, res) => {
---
+++ models/restaurant.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  
+  name_en: {
+    type: String,
+    required: true
+  },
+  
+  category: {
+    type: String,
+    required: true
+  },
+  
+  image: {
+    type: String,
+    required: false
+  },
+  
+  location: {
+    type: String,
+    required: false
+  },
+  
+  phone: {
+    type: String,
+    required: false
+  },
+  
+  google_map: {
+    type: String,
+    required: false
+  },
+  
+  rating: {
+    type: Number,
+    required: false
+  },
+  
+  description: {
+    type: String,
+    required: false
+  }
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
