089e011a69684bddfedb603be010793661082364
shibao <dream0184@yahoo.com.tw>
2021-11-07 14:27:22 +0800

    feat: add sort restaurant function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,21 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
+router.get('/sort/:order', (req, res) => {
+  const order = req.params.order
+  const selectedOrder = function(order) {
+    if (order === 'a-z') return { name_en: 'asc' }
+    if (order === 'z-a') return { name_en: 'desc' }
+    if (order === 'category') return { category: 'desc' }
+    if (order === 'location') return { location: 'desc' }
+  }
+  Restaurant.find()
+    .lean()
+    .sort( selectedOrder(order) )
+    .then(restaurants => res.render('index', { restaurantList: restaurants }))
+    .catch(error => console.error('error'))
+})
+
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
