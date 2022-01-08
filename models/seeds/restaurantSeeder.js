fc643aab53a0ccd3384953af51d6fed76bbddcca
shibao <dream0184@yahoo.com.tw>
2022-01-08 16:36:40 +0800

    refactor: refactor restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -15,6 +15,7 @@ const SEED_USER = [
     ownedRestaurant: [1, 2, 3]
   },
   {
+    name: 'user2',
     email: 'user2@example.com',
     password: '12345678',
     ownedRestaurant: [4, 5, 6]
@@ -23,34 +24,39 @@ const SEED_USER = [
 
 db.once('open', () => {
   console.log('mongodb connected!')
-
-  for (let i = 0; i < SEED_USER.length; i++) {
-    bcrypt.genSalt(10)
-      .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
-      .then(hash => User.create({
-        name: SEED_USER[i].name,
-        email: SEED_USER[i].email,
-        password: hash
-      }))
-      .then(user => {
-        const restaurantList = restaurants.filter(restaurant => SEED_USER[i].ownedRestaurant.includes(restaurant.id))
-
-        for (let i = 0; i < 3; i++) {
-          Restaurant.create({
-            name: restaurantList[i].name,
-            name_en: restaurantList[i].name_en,
-            category: restaurantList[i].category,
-            image: restaurantList[i].image,
-            phone: restaurantList[i].phone,
-            google_map: restaurantList[i].google_map,
-            rating: restaurantList[i].rating,
-            description: restaurantList[i].description,
-            userId: user._id
-          })
-        }
+  Promise.all(SEED_USER.map(user => {
+    return bcrypt.genSalt(10)
+      .then(salt => {
+        return bcrypt.hash(user.password, salt)
       })
-      .catch(err => console.log(err))
-  }
-  console.log('added restaurants to db')
-  process.exit()
+      .then(hash => {
+        return User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        })
+      })
+      .then(registeredUser => {
+        const restaurantList = restaurants.filter(restaurant => user.ownedRestaurant.includes(restaurant.id))     
+        return Promise.all(restaurantList.map(restaurant => 
+          Restaurant.create({
+            name: restaurant.name,
+            name_en: restaurant.name_en,
+            category: restaurant.category,
+            image: restaurant.image,
+            phone: restaurant.phone,
+            google_map: restaurant.google_map,
+            rating: restaurant.rating,
+            description: restaurant.description,
+            userId: registeredUser._id
+          })
+        ))
+      }) 
+  }))
+    .then(() => {
+      console.log('added restaurants to db')
+      process.exit()
+    })  
+    .catch(err => console.log(err))
 })
+
